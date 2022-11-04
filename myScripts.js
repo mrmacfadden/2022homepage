4ebb39480b690d679e37fde877e92f3de42b1a71
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2022-11-04 13:44:34 -0500

    Nov 4

---
+++ myScripts.js
@@ -0,0 +1,28 @@
+//CLOCK SCRIPT
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+   }
+   
+//POST-IT NOTE (JAVASCRIPT)
+if (localStorage["note"]) {
+    var note = localStorage["note"];
+    document.getElementById("note").value = note;
+}
+   
+   function autoSave() {
+    var note = document.getElementById("note").value;
+    localStorage.setItem("note", note);
+}
+   
