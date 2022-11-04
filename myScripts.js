3c098c53245d13cb65781742017fc6c232b5a01c
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2022-11-04 15:21:36 -0500

    Nov 4

--- myScripts.js
+++ myScripts.js
@@ -25,4 +25,17 @@ if (localStorage["note"]) {
     var note = document.getElementById("note").value;
     localStorage.setItem("note", note);
 }
-   
+
+var show = false;
+function showNote(){
+    if (show == false){
+        document.getElementById("note").style.opacity = 1;
+        show = true;
+    }
+    else if (show == true){
+        document.getElementById("note").style.opacity = 0;
+        show = false;
+    }
+ }
+
+ 
