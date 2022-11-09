141196836ad68c661dd18d0a576b959e453378b7
Mr. Macfadden <37188696+mrmacfadden@users.noreply.github.com>
2022-11-09 09:32:19 -0600

    Nov 9

--- myScripts.js
+++ myScripts.js
@@ -38,4 +38,18 @@ function showNote(){
     }
  }
 
+ //CALCULATOR (JAVASCRIPT)
+
+var showCalc = false;
+function showCalculator(){
+    if (showCalc == false){
+        document.getElementById("calc").style.opacity = 1;
+        showCalc = true;
+    }
+    else if (showCalc == true){
+        document.getElementById("calc").style.opacity = 0;
+        showCalc = false;
+    }
+ }
+
  
