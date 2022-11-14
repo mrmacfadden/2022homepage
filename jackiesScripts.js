5e51fde2a1ccead4ee7f7d75975c98e264e0b442
Mike Macfadden <mikemacfadden@gmail.com>
2022-11-14 11:31:09 -0600

    Nov 14

--- jackiesScripts.js
+++ jackiesScripts.js
@@ -48,6 +48,66 @@ function showTwitter(){
  }
 
 
+
+//JACKIE'S NOTES
+if (localStorage["monday"]) {
+  var monday = localStorage["monday"];
+  document.getElementById("monday").value = monday;
+} 
+function saveMonday() {
+  var monday = document.getElementById("monday").value;
+  localStorage.setItem("monday", monday);
+}
+
+if (localStorage["tuesday"]) {
+  var tuesday = localStorage["tuesday"];
+  document.getElementById("tuesday").value = tuesday;
+}
+function saveTuesday() {
+  var tuesday = document.getElementById("tuesday").value;
+  localStorage.setItem("tuesday", tuesday);
+}
+
+if (localStorage["wednesday"]) {
+  var wednesday = localStorage["wednesday"];
+  document.getElementById("wednesday").value = wednesday;
+}
+function saveWednesday() {
+  var wednesday = document.getElementById("wednesday").value;
+  localStorage.setItem("wednesday", wednesday);
+}
+
+if (localStorage["thursday"]) {
+  var thursday = localStorage["thursday"];
+  document.getElementById("thursday").value = thursday;
+}
+function saveThursday() {
+  var thursday = document.getElementById("thursday").value;
+  localStorage.setItem("thursday", thursday);
+}
+
+if (localStorage["friday"]) {
+  var friday = localStorage["friday"];
+  document.getElementById("friday").value = friday;
+}
+function saveFriday() {
+  var friday = document.getElementById("friday").value;
+  localStorage.setItem("friday", friday);
+}
+
+
+var show = false;
+function showNote() {
+  if (show == false) {
+    document.getElementById("notes").style.opacity = '1';
+    show = true;
+  } else if (show == true) {
+    document.getElementById("notes").style.opacity = '0';
+    show = false;
+  }
+}
+
+/*
 //POST-IT NOTE (JAVASCRIPT)
 if (localStorage["note"]) {
     var note = localStorage["note"];
@@ -70,3 +130,4 @@ function showNote(){
         show = false;
     }
  }
+*/
